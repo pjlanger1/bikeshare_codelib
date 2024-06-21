5c1618f80bc29852a2fd5ba2777a8c5812b5c83c
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-06-21 02:51:44 -0400

    Update lambda_function.py

--- aws_suite/lambda1.1/lambda_function.py
+++ aws_suite/lambda1.1/lambda_function.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import requests
 
 def lambda_handler(event, context):
-    #GBFS
+    #GBFS bike feed 'link_to_ping'
     kwd = {'legacy_id':'na','last_reported':'999','num_bikes_available':-1,'num_bikes_disabled':-1,'num_ebikes_available':-1,'num_docks_available':-1,'num_docks_disabled':-1,'num_scooters_available':-1,'num_scooters_unavailable':-1,'is_renting':-1,'is_returning':0,'is_installed':0}
     a = requests.get(link_to_ping).json()
     dest = {}
@@ -14,15 +14,15 @@ def lambda_handler(event, context):
     csv_string = ""
     for key, values in dest.items():
         csv_string += f"{key},{','.join(map(str, values))}\n"
-    #print(csv_string[0:120])
+    
     update_time = datetime.now()
     update_time_1 = f"{update_time.year}_{update_time.month}_{update_time.day}_{update_time.hour}_{update_time.minute}.csv"
 
-    bucket_name = 'jsonpublicfeed'  # Replace with your S3 bucket name
+    bucket_name = 'jsonpublicfeed'
 
-    s3_key = update_time_1  # Replace with the desired S3 key for the file
+    s3_key = update_time_1
 
-    #Upload the CSV string as a file to S3
+    #upload the CSV string as a file to S3
     s3 = boto3.resource('s3')
     s3.Object(bucket_name, s3_key).put(Body=csv_string)
         
