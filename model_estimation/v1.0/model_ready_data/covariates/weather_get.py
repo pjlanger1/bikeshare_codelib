210b71b9700c309a46c5e2ba623089f94e5eeaf3
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-06-28 13:28:09 -0400

    Update weather_get.py

--- model_estimation/v1.0/model_ready_data/covariates/weather_get.py
+++ model_estimation/v1.0/model_ready_data/covariates/weather_get.py
@@ -81,6 +81,36 @@ def get_weather(path, freq):
             df[nc] = np.where(df[nc] > 0, 1, 0)
     return df
 
+def check_time_increments(df, date_column, start_date, end_date):
+    """
+    Checks if the DataFrame contains all 15-minute increments between two dates in a specified column.
+
+    Args:
+    df (pd.DataFrame): DataFrame containing the data.
+    date_column (str): Name of the column containing the datetime values.
+    start_date (str): Start date in 'YYYY-MM-DD HH:MM:SS' format.
+    end_date (str): End date in 'YYYY-MM-DD HH:MM:SS' format.
+
+    Returns:
+    bool: True if all 15-minute increments are present, False otherwise.
+    """
+    
+    if not pd.api.types.is_datetime64_any_dtype(df[date_column]):
+        df[date_column] = pd.to_datetime(df[date_column])
+
+   
+    expected_times = pd.date_range(start=start_date, end=end_date, freq='15T')
+
+    actual_times = df[date_column].sort_values().unique()
+    actual_times = pd.to_datetime(actual_times)
+    missing_times = expected_times.difference(actual_times)
+  
+    if len(missing_times) == 0:
+        return (True,0)
+    else:
+        #print(f"Missing timestamps: {missing_times}")
+        return (False,missing_times)
+
 def encode_cyclic(datetime_obj):
     max_dict = {'month': 12, 'day': 31, 'hour': 24, 'minute': 60, 'day_of_week': 7}
     ll = {}
