56605c9e1a62bec19a89d865894ae5576279c345
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-11-13 16:16:51 -0500

    Create dag_stage3.sql

---
+++ simulation_code/code/data_pipeline/dag_stage3.sql
@@ -0,0 +1,43 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS weather_data (
+    time TIMESTAMP,
+    max_temp FLOAT,
+    min_temp FLOAT,
+    temp FLOAT,
+    wind_chill FLOAT,
+    heat_index FLOAT,
+    precip FLOAT,
+    snow FLOAT,
+    snow_depth FLOAT,
+    wind_speed FLOAT,
+    wind_direction FLOAT,
+    wind_gust FLOAT,
+    visibility FLOAT,
+    cloud_cover FLOAT,
+    humid FLOAT,
+    clear_cond INT,
+    none_cond FLOAT,
+    overcast_cond INT,
+    partially_cloudy_cond INT,
+    rain_cond INT,
+    snow_cond FLOAT,
+    month_sin FLOAT,
+    month_cos FLOAT,
+    day_sin FLOAT,
+    day_cos FLOAT,
+    hour_sin FLOAT,
+    hour_cos FLOAT,
+    minute_sin FLOAT,
+    minute_cos FLOAT,
+    day_of_week_sin FLOAT,
+    day_of_week_cos FLOAT,
+    dark INT,
+    part_dark INT,
+    light INT,
+    holiday INT
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+    'serialization.format' = ','
+) 
+LOCATION 's3://cleanedcitibike/weather_sim_data'
+TBLPROPERTIES ('has_encrypted_data'='false');
