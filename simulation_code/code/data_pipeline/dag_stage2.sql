80fd1232e13bbb73d622c2f27217ac83d6934f8b
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-11-15 21:21:10 -0500

    Update dag_stage2.sql

--- simulation_code/code/data_pipeline/dag_stage2.sql
+++ simulation_code/code/data_pipeline/dag_stage2.sql
@@ -1,12 +1,14 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS distances (
-origin string
-destination string
-distance float
-angle float
+    origin STRING,
+    destination STRING,
+    distance FLOAT,
+    angle FLOAT
 )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
 WITH SERDEPROPERTIES (
-    'serialization.format' = ','
-) 
+    'serialization.format' = ',',
+    "skip.header.line.count" = "1"
+)
+STORED AS TEXTFILE
 LOCATION 's3://cleanedcitibike/sim_dist'
 TBLPROPERTIES ('has_encrypted_data'='false');
