c36dee1b9b66c321156cf092178ff6e4a0973e94
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2024-11-15 23:39:05 -0500

    Update working.sql

--- simulation_code/code/data_pipeline/working.sql
+++ simulation_code/code/data_pipeline/working.sql
@@ -69,4 +69,102 @@ ON
     bs.destination_station_id = rb_dest.station_id
     AND bs.start_time = FROM_UNIXTIME(rb_dest.last_reported);
 
+
+
+-- now this is the second to last
+
+, base_flows AS (
+    SELECT 
+        origin,
+        destination,
+        CAST(DATE_FORMAT(start_time, '%w') AS INT) AS day_of_week,
+        
+        -- Define time of day segments
+        CASE
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 6 AND 9 THEN 'morning'
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 10 AND 15 THEN 'afternoon'
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 16 AND 19 THEN 'evening'
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 20 AND 23 THEN 'night'
+            ELSE 'early_morning'
+        END AS time_of_day,
+        
+        -- Aggregate trip and availability metrics
+        SUM(total_trips) AS total_trips,                     -- Total trips for each segment
+        AVG(total_trips) AS avg_trip_count,                  -- Average trips for each segment
+        AVG(ride_length) AS avg_ride_length,                 -- Average ride length
+        AVG(distance) AS avg_distance,                       -- Average distance for each segment
+        AVG(angle) AS avg_angle,                             -- Average angle (optional for spatial analysis)
+        AVG(origin_bikes_available) AS avg_origin_bikes,     -- Average bikes available at origin
+        AVG(origin_docks_available) AS avg_origin_docks,     -- Average docks available at origin
+        AVG(destination_bikes_available) AS avg_dest_bikes,  -- Average bikes available at destination
+        AVG(destination_docks_available) AS avg_dest_docks   -- Average docks available at destination
+        
+    FROM 
+        od_distance_availability
+    GROUP BY 
+        origin, 
+        destination, 
+        CAST(DATE_FORMAT(start_time, '%w') AS INT),     -- Day of week (0 = Sunday, 6 = Saturday)
+        CASE
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 6 AND 9 THEN 'morning'
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 10 AND 15 THEN 'afternoon'
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 16 AND 19 THEN 'evening'
+            WHEN CAST(DATE_FORMAT(start_time, '%H') AS INT) BETWEEN 20 AND 23 THEN 'night'
+            ELSE 'early_morning'
+        END
+)
+
+
+--and this is the last
+
+SELECT 
+    bf.origin,
+    bf.destination,
+    bf.day_of_week,
+    bf.time_of_day,
+    bf.avg_trip_count,
+    bf.total_trips,
+    bf.avg_ride_length,
+    bf.avg_distance,
+    bf.avg_angle,
+    bf.avg_origin_bikes,
+    bf.avg_origin_docks,
+    bf.avg_dest_bikes,
+    bf.avg_dest_docks,
+    
+    -- Weather metrics as additional exogenous variables
+    w.temp AS temperature,
+    w.precip AS precipitation,
+    w.wind_speed AS wind_speed,
+    w.cloud_cover AS cloud_cover,
+    w.humid AS humidity,
+    
+    -- Sine and cosine cyclic encoding for month, day, and time
+    w.month_sin, 
+    w.month_cos, 
+    w.day_of_week_sin, 
+    w.day_of_week_cos,
+    w.hour_sin,
+    w.hour_cos
+    
+FROM 
+    base_flows AS bf
+JOIN 
+    weather_data AS w
+ON 
+    bf.time_of_day = CASE
+        WHEN CAST(DATE_FORMAT(w.time, '%H') AS INT) BETWEEN 6 AND 9 THEN 'morning'
+        WHEN CAST(DATE_FORMAT(w.time, '%H') AS INT) BETWEEN 10 AND 15 THEN 'afternoon'
+        WHEN CAST(DATE_FORMAT(w.time, '%H') AS INT) BETWEEN 16 AND 19 THEN 'evening'
+        WHEN CAST(DATE_FORMAT(w.time, '%H') AS INT) BETWEEN 20 AND 23 THEN 'night'
+        ELSE 'early_morning'
+    END
+    AND bf.day_of_week = CAST(DATE_FORMAT(w.time, '%w') AS INT)
+ORDER BY 
+    bf.origin, 
+    bf.destination, 
+    bf.day_of_week, 
+    bf.time_of_day;
+
+
   
